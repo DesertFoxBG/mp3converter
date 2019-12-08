7750345654c125300d5ea1182e3892c2cbda80f0
DesertFoxBG <jordan1711@abv.bg>
2019-12-08 13:17:34 +0200

    a LOT of style changes

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,19 @@ function downloadYT(res, url, format) {
   }
 }
 
-/* GET home page. */
+// GET home page.
 router.get('/', function (req, res, next) {
   res.render('index');
+});
+
+// GET pp page.
+router.get('/pp', function (req, res, next) {
+  res.render('pp');
+});
+
+// GET tos page.
+router.get('/tos', function (req, res, next) {
+  res.render('tos');
 });
 
 router.get('/youtube', function (req, res, next) {
@@ -44,17 +54,20 @@ router.get('/ytinfo', function (req, res, next) {
   ytdl.getInfo(url, (err, info) => {
     if (err) throw err;
     console.log(info.player_response.streamingData.adaptiveFormats);
-    var div_els = `<div class="checkbox"><form action="/ytdownload"><p style="font-weight: bold;">audio/mp3 | audio [Recommended]</p><input name="url" value="${url}" style="display: none;"><input name="format" value="mp3" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form></div>`;
+    // recommended
+    var div_els = `<h1> </h1><form action="/ytdownload"><p style="font-weight: bold;">audio/mp3 | audio <i class="fas fa-check-circle"></i></p><input name="url" value="${url}" style="display: none;"><input name="format" value="mp3" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`;
     for (var c = 0; c < info.player_response.streamingData.formats.length; c++) {
       let format = info.player_response.streamingData.formats[c];
       let quality = format.qualityLable;
-      div_els += `<div class="checkbox"><form action="/ytdownload"><p style="font-weight: bold;">${format.mimeType.substring(0, format.mimeType.indexOf(';'))} ${quality ? '| ' + format.qualityLable : '| ' + format.quality} [Recommended]</p><input name="url" value="${format.url}" style="display: none;"><input name="format" value="${format.mimeType.substring(format.mimeType.indexOf('/') + 1, format.mimeType.indexOf(';'))}" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form></div>`
+      // recommended
+      div_els += `<form action="/ytdownload"><p style="font-weight: bold;">${format.mimeType.substring(0, format.mimeType.indexOf(';'))} ${quality ? '| ' + format.qualityLable : '| ' + format.quality} <i class="fas fa-check-circle"></i></p><input name="url" value="${format.url}" style="display: none;"><input name="format" value="${format.mimeType.substring(format.mimeType.indexOf('/') + 1, format.mimeType.indexOf(';'))}" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`
     }
     for (var c = 0; c < info.player_response.streamingData.adaptiveFormats.length; c++) {
       let format = info.player_response.streamingData.adaptiveFormats[c];
       let quality = format.qualityLable;
-      div_els += `<div class="checkbox"><form action="/ytdownload"><p style="font-weight: bold;">${format.mimeType.substring(0, format.mimeType.indexOf(';'))} ${quality ? '| ' + format.qualityLable : '| ' + format.quality}</p><input name="url" value="${format.url}" style="display: none;"><input name="format" value="${format.mimeType.substring(format.mimeType.indexOf('/') + 1, format.mimeType.indexOf(';'))}" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form></div>`
+      div_els += `<form action="/ytdownload"><p style="font-weight: bold;">${format.mimeType.substring(0, format.mimeType.indexOf(';'))} ${quality ? '| ' + format.qualityLable : '| ' + format.quality}</p><input name="url" value="${format.url}" style="display: none;"><input name="format" value="${format.mimeType.substring(format.mimeType.indexOf('/') + 1, format.mimeType.indexOf(';'))}" style="display: none;"><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`
     }
+    div_els += '<h1> </h1>'
     res.render('youtube', { info: div_els });
   });
 });
@@ -78,13 +91,14 @@ router.get('/fbinfo', function (req, res, next) {
   facebookdl.getInfo(url).then((info) => {
     var title = info.title;
     console.log(Object.keys(info.download));
-    var div_els = ``;
+    var div_els = `<h1> </h1>`;
     for(var c = 0; c < Object.keys(info.download).length; c++) {
       var key = Object.keys(info.download)[c];
       console.log(key, info.download[Object.keys(info.download)[c]]);
       if(info.download[Object.keys(info.download)[c]] == undefined) continue;
       div_els += `<form action="/fbdownload"><p style="font-weight: bold;">${title} | Quality: ${key.toLocaleUpperCase()}</p><input name="url" value="${info.download[key]}" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`;
     }
+    div_els += '<h1> </h1>'
 
     res.render('facebook', { info: div_els });
   });
@@ -116,26 +130,36 @@ router.get('/vimeo', function (req, res, next) {
 router.get('/vminfo', function (req, res, next) {
   var url = req.query.url;
 
-  var div_els = `<form action="/vmdownload"><p style="font-weight: bold;">Quality: 144p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="144p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 360p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="360p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 480p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="480p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 720p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="720p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 1080p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="1080p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`;
+  var div_els = `<h1> </h1><form action="/vmdownload"><p style="font-weight: bold;">Quality: 144p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="144p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 360p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="360p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 480p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="480p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 720p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="720p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><form action="/vmdownload"><p style="font-weight: bold;">Quality: 1080p</p><input name="url" value="${url}" style="display: none;"><input name="quality" value="1080p" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form>`;
+  div_els += '<h1> </h1>'
 
   res.render('vimeo', { info: div_els });
 });
 
-router.get('/vmdownload', function (req, res, next) {
+router.get('/vmdownload', function (req, res, next) { // fix .mp4 format
   var format = req.query.format;
   var url = req.query.url;
   var quality = req.query.quality;
 
-  vidl(url, { quality: quality })
+  vidl(url, { format: format, quality: quality })
     .pipe(fs.createWriteStream('./public/downloads/vimeo.' + format));
 
   res.attachment('./public/downloads/vimeo.' + format);
-  vidl(url, { quality: '360p' })
+  console.log('vimeo.' + format, quality);
+  vidl(url, { format: format, quality: quality })
     .pipe(res);
 });
 
 router.get('/tiktok', function (req, res, next) {
-  res.render('tiktok');
+  res.render('tiktok', { info: '' });
+});
+
+router.get('/ttinfo', function (req, res, next) {
+  var url = req.query.url;
+
+  var div_els = `<h1> </h1><form action="/ttdownload"><p style="font-weight: bold;">720p</p><input name="url" value="${url}" style="display: none;"><select name="format"><option value="mp3">MP3</option><option value="mp4">MP4</option></select><input type="submit" value="Download" style="margin: 0; position: relative;"></form><h1> </h1>`;
+  
+  res.render('tiktok', { info: div_els });
 });
 
 router.get('/ttdownload', function (req, res, next) {
--- tmp.js
+++ tmp.js
@@ -1,12 +1,5 @@
-'use strict';
- 
-let tt = require('twitter-dl');
- 
-let folder = './';
-let video = 'https://twitter.com/DierksBentley/status/703222600919588864';
- 
-tt.download(video, folder).then((result) => {
-  console.log(result);
-}).catch((err) => {
-  console.log(err);
+var req = require('request');
+
+var res = req.post('https://ripsave.com/download?video=', { url: '1017' }, (error, cont, body) => {
+  console.log(body);
 });
